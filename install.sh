4238493ea76d0ad472530661cadeab5ad3639aa2
dr0n1 <1930774374@qq.com>
2024-05-13 17:19:48 +0800

    V1.0

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 # Email: 1930774374@qq.com
 
 [ $(id -u) != "0" ] && {
-	echo "请用root用户运行"
-	exit 1
+    echo "请用root用户运行"
+    exit 1
 }
 
 tm=$(date +'%Y%m%d %T')
@@ -16,18 +16,17 @@ RESET="\x1b[0m"
 STR="abcdefghijklnmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 function info() {
-	echo -e "${COLOR_G}[$tm] [Info] ${1}${RESET}"
+    echo -e "${COLOR_G}[$tm] [Info] ${1}${RESET}"
 }
 
 function error() {
-	echo -e "${COLOR_R}[$tm] [Error] ${1}${RESET}"
+    echo -e "${COLOR_R}[$tm] [Error] ${1}${RESET}"
 }
 
 function generate_password() {
     local password=""
-    for ((i=1; i<=12; i++))
-    do
-        num=$((RANDOM%${#STR}))
+    for ((i = 1; i <= 12; i++)); do
+        num=$((RANDOM % ${#STR}))
         tmp=${STR:num:1}
         password+=$tmp
     done
@@ -48,7 +47,7 @@ function mkdir_directory() {
 }
 
 function install_docker() {
-	if command -v docker &>/dev/null; then
+    if command -v docker &>/dev/null; then
         info "docker已安装"
         systemctl restart docker
         if ! docker node ls | grep -q "Leader"; then
@@ -60,42 +59,42 @@ function install_docker() {
         fi
     else
         info "未检测到docker，开始安装 Docker..."
-	    ubuntu_version=$(lsb_release -r | awk '{print substr($2,1,2)}')
-	    if [ $ubuntu_version -le 16 ]; then
-		    apt-get update
-		    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-		    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-		    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-		    apt-get update
-		    apt-get install -y docker-ce
-	    else
-		    curl -sSL https://get.daocloud.io/docker | sh
-		    if [ $? -ne 0 ]; then
-		        curl -fLsS https://get.docker.com/ | sh
-		    fi
-	    fi
+        ubuntu_version=$(lsb_release -r | awk '{print substr($2,1,2)}')
+        if [ $ubuntu_version -le 16 ]; then
+            apt-get update
+            apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+            curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+            add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+            apt-get update
+            apt-get install -y docker-ce
+        else
+            curl -sSL https://get.daocloud.io/docker | sh
+            if [ $? -ne 0 ]; then
+                curl -fLsS https://get.docker.com/ | sh
+            fi
+        fi
 
-	    if ! command -v docker &>/dev/null; then
-		    error "可能由于网络原因或其他未知原因导致Docker安装失败，请检查后重试"
-		    exit 1
-	    fi
+        if ! command -v docker &>/dev/null; then
+            error "可能由于网络原因或其他未知原因导致Docker安装失败，请检查后重试"
+            exit 1
+        fi
 
-	    systemctl enable docker
-	    systemctl restart docker
-	    docker swarm init --advertise-addr 127.0.0.1
+        systemctl enable docker
+        systemctl restart docker
+        docker swarm init --advertise-addr 127.0.0.1
         docker node update --label-add='name=linux-1' $(docker node ls -q)
-	fi
+    fi
 }
 
 function install_dockercompose() {
     if command -v docker-compose &>/dev/null; then
-		info "docker-compose已安装"
+        info "docker-compose已安装"
     else
         info "开始安装docker-compose"
         curl -sSL https://get.daocloud.io/docker | sh
         curl -SL https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-x86_64 -o /usr/bin/docker-compose
         chmod +x /usr/bin/docker-compose
-	fi
+    fi
 }
 
 function download_ctfd() {
@@ -122,12 +121,11 @@ function install_plugins() {
     [ ! -n "${choice1}" ] && { choice1="yes"; }
     if [[ "$choice1" == "yes" || "$choice1" == "y" ]]; then
         install_plugins_whale
-    elif [[ "$choice1" == "no" || "$choice1" == "n"  ]]; then
+    elif [[ "$choice1" == "no" || "$choice1" == "n" ]]; then
         info "跳过 [ctfd-whale]"
     else
         error "无效的选择"
     fi
-
 
     read -p "是否添加 [ctfd-pages-theme] 插件？(yes/no): " choice2
     choice2=$(echo "$choice2" | tr '[:upper:]' '[:lower:]')
@@ -144,7 +142,7 @@ function install_plugins() {
 function install_plugins_whale() {
     info "开始下载 [ctfd-whale]"
     git clone --depth 1 https://github.com/frankli0324/ctfd-whale CTFd/plugins/ctfd-whale
-    echo "flask_apscheduler" >> requirements.txt
+    echo "flask_apscheduler" >>requirements.txt
     curl -fsSL https://raw.githubusercontent.com/frankli0324/ctfd-whale/master/docker-compose.example.yml -o docker-compose.yml
 
     info "开始设置参数"
@@ -179,7 +177,8 @@ function install_plugins_pages() {
     git clone https://github.com/frankli0324/ctfd-pages-theme CTFd/themes/pages
 
     info "开始添加路由"
-    data=$(cat <<EOM
+    data=$(
+        cat <<EOM
 
 @challenges_namespace.route("/categories")
 class ChallengeCategories(Resource):
@@ -190,9 +189,9 @@ class ChallengeCategories(Resource):
             chal_q = chal_q.filter(and_(Challenges.state != "hidden", Challenges.state != "locked"))
         return {"success": True, "data": [i.category for i in chal_q]}
 EOM
-)
+    )
 
-    echo "$data" >> ./CTFd/api/v1/challenges.py
+    echo "$data" >>./CTFd/api/v1/challenges.py
 }
 
 function build_ctfd() {
